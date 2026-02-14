b744930710e123166aac964b428b9ba7b1c03a55
JulioEguis <144676330+JulioEguis@users.noreply.github.com>
2026-02-14 20:40:05 +0100

    Update script to include system update and Apache install
    Added system update and Apache installation steps.

--- instalacion_pila_lamp.sh
+++ instalacion_pila_lamp.sh
@@ -8,3 +8,15 @@ if [ "$EUID" -ne 0 ]; then
     echo "ejecuta este script como root o usando sudo"
     exit 1
 fi
+
+# Actualizar sistema y que no pida la confirmación
+echo "Actualizando el sistema..."
+apt update && apt upgrade -y
+
+# Instalar Apache
+echo "Instalando Apache..."
+apt install apache2 -y
+systemctl enable apache2
+systemctl start apache2
+
+
