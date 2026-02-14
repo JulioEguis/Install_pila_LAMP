34c09215821b3359a12836a848887a888ba9858e
JulioEguis <144676330+JulioEguis@users.noreply.github.com>
2026-02-14 20:39:03 +0100

    Add root user check to instalacion_pila_lamp.sh
    Added root user check to the LAMP installation script.

--- instalacion_pila_lamp.sh
+++ instalacion_pila_lamp.sh
@@ -2,3 +2,9 @@
 
 # Script de instalación de LAMP en Linux
 echo "Instalador de LAMP para Ubuntu"
+
+# Verificar si se ejecuta como root
+if [ "$EUID" -ne 0 ]; then 
+    echo "ejecuta este script como root o usando sudo"
+    exit 1
+fi
